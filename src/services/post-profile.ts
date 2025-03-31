6127bb6fd74bac4e7ec3693a95e0126113fd3b16
tpedrozo <mr.thiagocesar@hotmail.com>
2025-03-31 09:52:47 -0300

    refactor: correct API endpoint URL in postProfile function for proper request handling

--- src/services/post-profile.ts
+++ src/services/post-profile.ts
@@ -2,11 +2,15 @@ import { IProfile } from "@/models/profile.model";
 import axios from "axios";
 
 export async function postProfile(data: IProfile) {
-  const response = await axios.post("4me-quiz-backend.vercel.app/lead", data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.post(
+    "https://4me-quiz-backend.vercel.app/lead",
+    data,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   return response.data;
 }
