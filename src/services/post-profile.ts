b0e9142154d4bf7d9f588a46812222f81461343d
tpedrozo <mr.thiagocesar@hotmail.com>
2025-03-31 09:21:35 -0300

    refactor: update API endpoint in postProfile function for improved backend integration

--- src/services/post-profile.ts
+++ src/services/post-profile.ts
@@ -2,15 +2,11 @@ import { IProfile } from "@/models/profile.model";
 import axios from "axios";
 
 export async function postProfile(data: IProfile) {
-  const response = await axios.post(
-    "https://4me.methods.com.br/api/lead",
-    data,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await axios.post("4me-quiz-backend.vercel.app/lead", data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   return response.data;
 }
