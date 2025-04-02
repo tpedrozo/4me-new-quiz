85393c73c673188e9a66d08b6c31056e295fcb59
tpedrozo <mr.thiagocesar@hotmail.com>
2025-04-02 11:08:58 -0300

    refactor: update API endpoint in postProfile function to use environment variable for better configuration

--- src/services/post-profile.ts
+++ src/services/post-profile.ts
@@ -2,11 +2,15 @@ import { IProfile } from "@/models/profile.model";
 import axios from "axios";
 
 export async function postProfile(data: IProfile) {
-  const response = await axios.post("http://localhost:3000/lead", data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/lead`,
+    data,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   return response.data;
 }
